8a0be459dd5ad6caad0a7a2036096004e03636dc
yogii30 <92358002+yogii30@users.noreply.github.com>
2023-04-03 16:10:50 +0530

    Create notification-count.js

---
+++ notification-count.js
@@ -0,0 +1,28 @@
+class NotificationCount {
+	constructor(webflowMemberId){
+		this.webflowMemberId = webflowMemberId;
+		this.getNotificationData();
+	}
+	displayUnreadMessage(messageData){
+		var notificationBudge = document.getElementsByClassName("notification-budge")[0];
+		var unreadMessage = messageData.filter(data => !data.is_read)
+		//notificationBudge.innerHTML = unreadMessage.length;
+		notificationBudge.setAttribute('data-count', unreadMessage.length)
+		console.log('notificationBudge', notificationBudge)
+	}
+	getNotificationData(){
+		var xhr = new XMLHttpRequest()
+		var $this = this;
+		xhr.open("GET", "https://3yf0irxn2c.execute-api.us-west-1.amazonaws.com/dev/camp/getNotifications/"+$this.webflowMemberId, true)
+		xhr.withCredentials = false
+		xhr.send()
+		xhr.onload = function() {
+			let responseText =  JSON.parse(xhr.responseText);
+			$this.displayUnreadMessage(responseText)
+		}
+	}
+}
+
+(function() {
+    new NotificationCount(webflowMemberId)
+})();
