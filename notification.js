b2c4440695810b98246e4d47f5b5973f451ed81a
yogii30 <92358002+yogii30@users.noreply.github.com>
2023-04-06 16:41:48 +0530

    Update notification.js

--- notification.js
+++ notification.js
@@ -15,6 +15,7 @@ class Notification {
 		this.messageData = messageData;
 		this.filterData = messageData;
 		this.paginateData = this.paginatorList(messageData);
+		
 		this.makeMessageFilter();
 		this.makeMessageList();
 		this.displayUnreadMessage();
@@ -43,7 +44,6 @@ class Notification {
 		if(notificationCount){
 			notificationCount.remove();
 		}
-		console.log('this.messageData', this.messageData)
 		var unreadMessage = this.filterData.filter(data => !data.is_read)
 		var notificationMessage = creEl('span', 'notification-count');
 		notificationMessage.innerHTML = unreadMessage.length;
@@ -180,6 +180,46 @@ class Notification {
 		img.src = src;
 		return img
 	}
+	download(fileLink, fileName){
+		fetch(fileLink)
+		  .then(resp => resp.blob())
+		  .then(blob => {
+			const url = window.URL.createObjectURL(blob);
+			const a = document.createElement('a');
+			a.style.display = 'none';
+			a.href = url;
+			// the filename you want
+			a.download = fileName;
+			document.body.appendChild(a);
+			a.click();
+			window.URL.revokeObjectURL(url);
+			//alert('your file has downloaded!'); // or you know, something with better UX...
+		  })
+		  .catch(() => alert('oh no!'));
+	}
+	downLoadLinkIcon(fileLink){
+		var $this = this;
+		var fileName = fileLink
+		var a = creEl('a', 'downloadLink')
+		var img = creEl('img', 'downloadIcon')
+		img.src="https://uploads-ssl.webflow.com/6271a4bf060d543533060f47/642e8f4e85af6b0ef1e409b6_link-solid.png";
+		a.appendChild(img);
+		a.addEventListener('click', function () {
+				$this.download(fileLink, fileLink.substring(fileLink.lastIndexOf('/')+1));
+		})
+		return a;
+	}
+	viewDownLoadedFile(fileLink){
+		var $this = this;
+		var fileName = fileLink
+		var a = creEl('a', 'downloadLink iframe-lightbox-link')
+		var img = creEl('img', 'downloadIcon')
+		img.src="https://uploads-ssl.webflow.com/6271a4bf060d543533060f47/642ea24ad58cc7503128b575_eye-solid.png";
+		a.appendChild(img);
+		a.href = fileLink;
+		
+		return a;
+	}
 	craeteMessageList(){
 		var $this = this;
 		var messageList = creEl('div', 'message-list');
@@ -187,13 +227,19 @@ class Notification {
 			var is_read = (item.is_read) ? 'read' : 'not-read';
 			var row = creEl('div', 'w-row '+is_read)
 			
+			
+			
+			
 			var read_icon = this.getCheckedIcon(item.is_read); 
 			var col_icon = this.createCol('', 1);
+			
 			col_icon.appendChild(read_icon);
 			row.appendChild(col_icon);
 			
 			var col_1 = this.createCol(item.title);
 			row.appendChild(col_1);
+			
+			
 			var col_2 = this.createCol(item.type, 1);
 			row.appendChild(col_2);
 			var col_3 = this.createCol(item.sendAs, 2);
@@ -201,12 +247,19 @@ class Notification {
 			var col_4 = this.createCol(item.message.substring(0, 30) + '...', 3);
 			row.appendChild(col_4);
 			var col_5 = this.createCol(this.formatedDate(item.created_on), 2);
+			
+			//Append Download Icon
+			if(item.uploadedFiles){
+				var downloadIcon = this.downLoadLinkIcon(item.uploadedFiles);
+				col_5.appendChild(downloadIcon);
+			}
 			row.appendChild(col_5);
 			row.addEventListener('click', function () {
 				$this.displayDetailsPage(item);
 			})
 			messageList.appendChild(row)
 		})
+		
 		return messageList;
 	}
 	createMessageTitle(){
@@ -296,6 +349,17 @@ class Notification {
 		var dateMessagecol = creEl("div", 'w-col w-col-12 details-message');
 		dateMessagecol.innerHTML = message;
 		contain.appendChild(dateMessagecol);
+		
+		if(item.uploadedFiles){
+			var viewIcon = this.viewDownLoadedFile(item.uploadedFiles)
+			var downloadCol = creEl("div", 'w-col w-col-12 download-icon');
+			var download_head = this.creBoldText('Attached File: ')
+			var downloadIcon = this.downLoadLinkIcon(item.uploadedFiles);
+			downloadCol.appendChild(download_head);
+			downloadCol.appendChild(downloadIcon);
+			downloadCol.appendChild(viewIcon);
+			contain.appendChild(downloadCol);
+		}
 
 		
 		return contain;
@@ -352,6 +416,7 @@ class Notification {
 		/*Back button for detail page*/
 		var backButton = this.detailPageBackButton();
 		notificationDetails.appendChild(backButton);
+		this.initiateLightbox();
 
 		
 	}
@@ -411,6 +476,18 @@ class Notification {
 		
 		var pagination = this.createPagination();
 		notification.appendChild(pagination);
+		
+	}
+	initiateLightbox(){
+		console.log('testing');
+		[].forEach.call(document.getElementsByClassName("iframe-lightbox-link"), function (el) {
+		  el.lightbox = new IframeLightbox(el, {
+			onClosed: function() {
+				
+			},
+			scrolling: true,
+		  });
+		});
 	}
 	
 }
@@ -429,7 +506,7 @@ class NotificationApi {
 		xhr.send()
 		xhr.onload = function() {
 			let responseText =  JSON.parse(xhr.responseText);
-			new Notification($this.webflowMemberId, responseText);  
+			new Notification($this.webflowMemberId, responseText); 			
 		}
 	}
 }
