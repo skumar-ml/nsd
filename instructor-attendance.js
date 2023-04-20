a04d969b0f33da448716fb091ecb10c6ce95e52a
yogii30 <92358002+yogii30@users.noreply.github.com>
2023-04-20 19:37:50 +0530

    Update instructor-attendance.js

--- instructor-attendance.js
+++ instructor-attendance.js
@@ -1,4 +1,3 @@
-/*Attendance JS*/
 /**
  * 	
  * @param name - HTML element name
@@ -18,10 +17,156 @@ function creEl(name,className,idName){
 
 class selfCheckInForm {
 	$currentLab = {};
+	$currentLabStudent = {};
 	constructor(webflowMemberId, labsData){
 		this.webflowMemberId = webflowMemberId;
 		this.labsData = labsData.labs;
 		this.view();
+	}
+	/*Creating pagination array object*/
+	paginatorList(items, page, per_page) {
+		var page = page || 1,
+		per_page = per_page || 5,
+		offset = (page - 1) * per_page,
+
+		paginatedItems = items.slice(offset).slice(0, per_page),
+		total_pages = Math.ceil(items.length / per_page);
+		return {
+			page: page,
+			per_page: per_page,
+			pre_page: page - 1 ? page - 1 : null,
+			next_page: (total_pages > page) ? page + 1 : null,
+			total: items.length,
+			total_pages: total_pages,
+			data: paginatedItems
+		};
+	}
+	/* Creating the DOM element for date filter like new and old */
+	createInstructorCheckInFilter(){
+		var $this = this;
+		var col = creEl("div", 'col');
+		var label = creEl("label", 'form-field-label')
+		label.innerHTML = "Instructor";
+		col.appendChild(label)
+		
+		var dateFilter = creEl('select', 'ins-checkin-filter w-select', 'ins-checkin-filter');
+		var defaultoption = creEl("option");
+		defaultoption.value = "";
+		defaultoption.text = "Select";
+		dateFilter.appendChild(defaultoption);
+		//Newest
+		var newestoption = creEl("option");
+		newestoption.value = "true";
+		newestoption.text = "CheckedIn";
+		dateFilter.appendChild(newestoption);
+		//oldest
+		var oldestoption = creEl("option");
+		oldestoption.value = "false";
+		oldestoption.text = "Not Checked In";
+		dateFilter.appendChild(oldestoption);
+		col.appendChild(dateFilter)
+		dateFilter.addEventListener('change', function () {
+			$this.filterstudentData('type', this.value);
+		})
+		return col;
+	}
+	/* Creating the DOM element for date filter like new and old */
+	createStudentCheckInFilter(){
+		var $this = this;
+		var col = creEl("div", 'col');
+		var label = creEl("label", 'form-field-label')
+		label.innerHTML = "Student";
+		col.appendChild(label)
+		
+		var dateFilter = creEl('select', 'student-checkin-filter w-select', 'student-checkin-filter');
+		var defaultoption = creEl("option");
+		defaultoption.value = "";
+		defaultoption.text = "Select";
+		dateFilter.appendChild(defaultoption);
+		//Newest
+		var newestoption = creEl("option");
+		newestoption.value = "true";
+		newestoption.text = "CheckedIn";
+		dateFilter.appendChild(newestoption);
+		//oldest
+		var oldestoption = creEl("option");
+		oldestoption.value = "false";
+		oldestoption.text = "Not Checked In";
+		dateFilter.appendChild(oldestoption);
+		col.appendChild(dateFilter)
+		dateFilter.addEventListener('change', function () {
+			$this.filterstudentData('type', this.value);
+		})
+		return col;
+	}
+	/* Creating dom element for search filter*/
+	createSearchFilter(){
+		var $this = this;
+		var col = creEl("div", 'col');
+		
+		var label = creEl("label", 'form-field-label')
+		label.innerHTML = "Search";
+		col.appendChild(label)
+		
+		
+		var searchFilter = creEl('input', 'search-filter form-text-label w-input', 'search-filter')
+		searchFilter.name = 'messageSearch';
+		searchFilter.placeholder = "Student";
+		/*Event for search*/
+		searchFilter.addEventListener('keypress', function (event) {
+			if (event.key === "Enter") {
+				$this.filterstudentData('type', this.value);
+			}
+		})
+		
+		col.appendChild(searchFilter)
+		return col;
+	}
+	/* Creating dom element for filter header */
+	makeAttendanceFilter(){
+		//var attendanceFilter = document.getElementById("attendance-filter");
+		/*Filter*/
+		var attendanceHeader = creEl('div', 'attendance-header w-layout-grid grid-3', 'attendance-header')
+		var instructorFilter = this.createInstructorCheckInFilter();
+		var studentFilter = this.createStudentCheckInFilter();
+		var searchFilter = this.createSearchFilter();
+		attendanceHeader.appendChild(instructorFilter);
+		attendanceHeader.appendChild(studentFilter);
+		attendanceHeader.appendChild(searchFilter);
+		//attendanceFilter.appendChild(attendanceHeader);
+		return attendanceHeader;
+	}
+	/*Filter api response based on current seleted filter value*/
+	filterstudentData(){
+		//this.studentData = this.filterData;
+		var studentData = this.$currentLab.student;
+		
+		var insCheckinFilter = document.getElementById("ins-checkin-filter");
+		
+		var studentCheckinFilter = document.getElementById("student-checkin-filter");
+		
+		var searchFilter = document.getElementById("search-filter");
+		console.log('studentData', studentData)
+		console.log('insCheckinFilter.value', insCheckinFilter.value)
+		if(insCheckinFilter.value){
+			console.log('insCheckinFilter sss')
+			studentData = studentData.filter(item => item.isICheckedIn.toString() == insCheckinFilter.value)
+		}
+		if(studentCheckinFilter.value){
+			studentData = studentData.filter(item => item.isCheckedIn.toString() == studentCheckinFilter.value)
+		}
+		
+		if(searchFilter.value){
+			var search = searchFilter.value;
+			var condition = new RegExp(search, 'i');
+			studentData = studentData.filter(function (el) {
+			  return condition.test(el.name);
+			});
+		}
+		console.log('studentData', studentData)
+		this.$currentLab = studentData;
+		this.$currentLabStudent = this.paginatorList(studentData)
+		this.refreshData();
 	}
 	/* Creating dom element for column based on column width*/
 	createCol(message, col_width){
@@ -51,12 +196,21 @@ class selfCheckInForm {
 		var labData = this.getLabs();
 		col.appendChild(labData)
 		
+		var studentlistfilter = creEl("div", 'student-list-filter', 'student-list-filter');
+		var filter = this.makeAttendanceFilter();
+		studentlistfilter.appendChild(filter);
+		col.appendChild(studentlistfilter)
+		
 		var head = this.createAttendanceTitle();
 		col.appendChild(head)
 		
 		
+		
 		var studentlist = creEl("div", 'student-list', 'student-list');
 		col.appendChild(studentlist)
+		
+		var pagination = creEl("div", "pagination-student-list", 'pagination-student-list')
+		col.appendChild(pagination)
 		
 		row.appendChild(col)
 		accordionDiv.appendChild(row);
@@ -78,7 +232,7 @@ class selfCheckInForm {
 		})
 		
 		labsSelectBox.addEventListener('change', function () {
-			$this.displayStudentList(this.value);
+			$this.displayStudentList(this.value, 'init');
 		})
 		
 		return labsSelectBox;
@@ -99,19 +253,30 @@ class selfCheckInForm {
 		})
 		return row;
 	}
-	displayStudentList(labId){
+	displayStudentList(labId, type=''){
 		
 		var opacity = (labId) ? 1 : 0;
+		
 		var btn = document.getElementsByClassName('student-list-head')[0];
 		btn.style.opacity = opacity;
 		btn.style.transition = 'all 2s';
+		
+		var studentlistfilter = document.getElementsByClassName('student-list-filter')[0];
+		studentlistfilter.style.opacity = opacity;
+		studentlistfilter.style.transition = 'all 2s';
 		
 		var studentlist = document.getElementById('student-list');
 		studentlist.innerHTML = "";
 		var $this = this;
 		var currentLab = this.labsData.find(item => item.id == labId);
+		if(type == 'init'){
+			var currentLabStudent = this.paginatorList(currentLab.student)
+		}else{
+			var currentLabStudent = this.$currentLabStudent
+		}
 		this.$currentLab = currentLab;
-		currentLab.student.forEach((item, index) => {
+		this.$currentLabStudent = currentLabStudent;
+		currentLabStudent.data.forEach((item, index) => {
 			var row = creEl('div', 'w-row')
 			
 			var col_1 = this.createCol(item.name,6);
@@ -141,23 +306,29 @@ class selfCheckInForm {
 			row.appendChild(col_3);
 			studentlist.appendChild(row)
 		})
-		return studentlist;
+		
+		//Pagination
+		var paginationStuList = document.getElementById('pagination-student-list');
+		paginationStuList.innerHTML = "";
+		var pagination = this.createPagination();
+		paginationStuList.appendChild(pagination);
+		
+		//return studentlist;
 	}
 	updateAttendanceData(studentId){
 		var labsData = this.labsData;
 		var currentLab = this.$currentLab;
-		labsData.map(item =>{
-			if(item.id == currentLab.id){
-				item.student =  item.student.map(sItem => {
+		//currentLab.map(item =>{
+				currentLab.student.map(sItem => {
 					if(sItem.id == studentId){
 						sItem.isICheckedIn = true;
 					}
 					return sItem;
 				})
-			}
-			return item;
-		})
-		this.$currentLab = labsData
+		//})
+		console.log('currentLab', currentLab)
+		this.$currentLab = currentLab
+		this.$currentLabStudent = this.paginatorList(currentLab.student);
 	}
 	getCheckInIcon(){
 		var img = creEl('img', 'checkedInIcon')
@@ -199,10 +370,46 @@ class selfCheckInForm {
 		})
 		this.labsData = labsData;
 	}
+	/* Creating dom element pagination */
+	createPagination(){
+		var $this = this;
+		var pagination = creEl('div', 'w-pagination-wrapper', 'notification-body');
+		/*Previous Button*/
+		console.log('this.$currentLabStudent', this.$currentLabStudent)
+		if(this.$currentLabStudent.pre_page != null){
+			var preBtn = creEl('a', 'w-pagination-previous');
+			preBtn.innerHTML = '< Previous';
+			preBtn.addEventListener('click', function () {
+				$this.$currentLabStudent = $this.paginatorList($this.$currentLab.student, $this.$currentLabStudent.pre_page);
+				$this.refreshData();
+			})
+			pagination.appendChild(preBtn);
+		}
+		/*Next Button*/
+		if(this.$currentLabStudent.next_page != null){
+			var nextBtn = creEl('a', 'w-pagination-next');
+			nextBtn.innerHTML = 'Next >';
+			nextBtn.addEventListener('click', function () {
+				$this.$currentLabStudent = $this.paginatorList($this.$currentLab.student, $this.$currentLabStudent.next_page);
+				$this.refreshData();
+			})
+			pagination.appendChild(nextBtn);
+		}
+		
+		return pagination;
+	}
+	refreshData(){
+		var studentlist = document.getElementById('student-list');
+		var paginationStuList = document.getElementById('pagination-student-list');
+		studentlist.innerHTML = "";
+		paginationStuList.innerHTML = "";
+		var labsSelectBox = document.getElementById('select-labs');
+		this.displayStudentList(labsSelectBox.value);
+	}
 }
 class LabsData {
 	$isLoading = true;
-	$messageData = '';
+	$studentData = '';
 	constructor(webflowMemberId){
 		this.webflowMemberId = webflowMemberId;
 		this.getLabsData();
@@ -215,7 +422,7 @@ class LabsData {
 		xhr.send()
 		xhr.onload = function() {
 			//let responseText =  JSON.parse(xhr.responseText);
-			let responseText =  JSON.parse('{"labs":[{"name":"Lab 1","id":"97427492749274927342","student":[{"name":"Yogesh Yadav","id":"1","isCheckedIn":false,"isICheckedIn":false},{"name":"Dev Narayan","id":"2","isCheckedIn":false,"isICheckedIn":false}]},{"name":"Lab 2","id":"97427492749274927333","student":[{"name":"Abha","id":"1","isCheckedIn":false,"isICheckedIn":false},{"name":"Shubham","id":"2","isCheckedIn":false,"isICheckedIn":false}]}]}');
+			let responseText =  JSON.parse('{"labs":[{"name":"Lab 1","id":"97427492749274927342","student":[{"name":"Yogesh Yadav","id":"1","isCheckedIn":true,"isICheckedIn":false},{"name":"Dev Narayan","id":"2","isCheckedIn":false,"isICheckedIn":false},{"name":"Rahul","id":"2","isCheckedIn":false,"isICheckedIn":false},{"name":"Aryan","id":"3","isCheckedIn":true,"isICheckedIn":false},{"name":"Drishti","id":"4","isCheckedIn":false,"isICheckedIn":false},{"name":"Abha","id":"5","isCheckedIn":false,"isICheckedIn":false},{"name":"shiv","id":"6","isCheckedIn":true,"isICheckedIn":false},{"name":"ajay","id":"7","isCheckedIn":false,"isICheckedIn":false},{"name":"ram","id":"8","isCheckedIn":false,"isICheckedIn":false}]},{"name":"Lab 2","id":"97427492749274927333","student":[{"name":"aryan","id":"9","isCheckedIn":false,"isICheckedIn":false},{"name":"devendra","id":"10","isCheckedIn":true,"isICheckedIn":false},{"name":"Shubham","id":"11","isCheckedIn":false,"isICheckedIn":false},{"name":"rahul","id":"12","isCheckedIn":false,"isICheckedIn":false},{"name":"mukesh","id":"13","isCheckedIn":true,"isICheckedIn":false},{"name":"manju","id":"14","isCheckedIn":false,"isICheckedIn":false},{"name":"sourabh","id":"15","isCheckedIn":false,"isICheckedIn":false},{"name":"sheetal","id":"16","isCheckedIn":false,"isICheckedIn":false},{"name":"samir","id":"17","isCheckedIn":false,"isICheckedIn":false},{"name":"akhil","id":"18","isCheckedIn":true,"isICheckedIn":false},{"name":"rahi","id":"19","isCheckedIn":false,"isICheckedIn":false},{"name":"ram","id":"20","isCheckedIn":true,"isICheckedIn":false}]}]}');
 			new selfCheckInForm($this.webflowMemberId, responseText); 			
 		}
 	}
