bc2f20e305d82d4090bb96c60383962d86c327b0
sk1840939 <63837566+sk1840939@users.noreply.github.com>
2023-06-21 13:08:56 -0700

    LD & PF Skill Builder for All Events

--- registration_accordian.js
+++ registration_accordian.js
@@ -407,7 +407,16 @@ class AccordionForm {
 		else if (debateEvent === "Public Forum") {
 			skillBuilderLink = "https://drive.google.com/drive/folders/1ePKoI9HiyyDlJx0oKwQE1MZW81FejpDu?usp=sharing";
 		}
+		else if (debateEvent === "All Events") {
+			var allEventSkillLink = "https://drive.google.com/drive/folders/1xaIDK1lEBSYXMH86mIs1M7-Wlu2sBXCl?usp=sharing";
+			const allEventSkillHTML = '<a href=' + allEventSkillLink + ' target="_blank" class="portal-resource-card portal-div-shadow w-inline-block"><p class="portal-card-text">LD Skill Builder</p></a>';
+			resourceGrid.insertAdjacentHTML('beforeend', allEventSkillHTML);
 
+			var allEventSkillLink = "https://drive.google.com/drive/folders/1ePKoI9HiyyDlJx0oKwQE1MZW81FejpDu?usp=sharing";
+			const allEventSkillHTML = '<a href=' + allEventSkillLink + ' target="_blank" class="portal-resource-card portal-div-shadow w-inline-block"><p class="portal-card-text">PF Skill Builder</p></a>';
+			resourceGrid.insertAdjacentHTML('beforeend', allEventSkillHTML);
+		}
+			
 		if (skillBuilderLink !== "#") {
 			const skillBuilderHTML = '<a href=' + skillBuilderLink + ' target="_blank" class="portal-resource-card portal-div-shadow w-inline-block"><p class="portal-card-text">NSD Skill Builder</p></a>';
 			resourceGrid.insertAdjacentHTML('beforeend', skillBuilderHTML);
