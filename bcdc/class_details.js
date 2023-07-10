6a7c4349e0a3e08156a4c47e6d7a099dea969408
yogii30 <92358002+yogii30@users.noreply.github.com>
2023-07-10 19:15:06 +0530

    Update class_details.js

--- bcdc/class_details.js
+++ bcdc/class_details.js
@@ -174,7 +174,7 @@ class classLocation{
 				var spinner = document.getElementById('half-circle-spinner');
 				spinner.style.display = 'block';				
 				setTimeout(function() {
-					new classDetails(api, $this.webflowMemberId, $this.accountEmail, this.levelId)
+					new classDetails(api, $this.webflowMemberId, $this.accountEmail, $this.levelId)
 				}, 500);
 			},
 			scrolling: true,
