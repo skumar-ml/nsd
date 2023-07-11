c9b5f77290450065a7521ab160ee0be98a350ee0
yogii30 <92358002+yogii30@users.noreply.github.com>
2023-07-11 17:31:35 +0530

    Add files via upload

---
+++ bcdc/class_list.js
@@ -0,0 +1,61 @@
+class classLists {
+	constructor(baseUrl) {
+		this.baseUrl = baseUrl;
+		this.renderPortalData();
+	}
+	async fetchData(endpoint) {
+		try {
+			const response = await fetch(`${this.baseUrl}${endpoint}`);
+			if (!response.ok) {
+			throw new Error('Network response was not ok');
+			}
+			const data = await response.json();
+			
+			return data;
+		} catch (error) {
+			console.error('Error fetching data:', error);
+			throw error;
+		}
+	}
+	viewClassLocations(classData){
+		var timingElement = document.getElementsByClassName('location-dropdown-day-time');
+		for(let i=0; i<timingElement.length; i++){
+			let levelId = timingElement[i].getAttribute('levelId');
+			let location = timingElement[i].getAttribute('location');
+			var levelData = classData.filter(item=>item.levelId == levelId)	
+			levelData.forEach(item=>{
+				let locationData = item.location.filter(data => data.locationName == location)
+				//console.log('locationData', locationData)
+				if(locationData.length > 0){
+					let timingData = locationData[0].timing
+					var timingText = '';
+					timingData.forEach((timeData,index)=>{
+						timingText = timingText+' '+timeData.day+" "+timeData.startTime+"-"+timeData.endTime+"&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp";
+					})
+					console.log(levelId, location, timingText)
+					timingText = timingText.replace(/PM/g, "")
+					timingText = timingText.replace(/AM/g, "")
+					timingElement[i].innerHTML = timingText;
+				}else{
+					timingElement[i].parentElement.parentElement.parentElement.style.display = 'none'
+				}
+			})
+			
+		}
+		var spinner = document.getElementById('half-circle-spinner');
+		spinner.style.display = 'none';
+	}
+	async renderPortalData() {
+		try {
+		  var spinner = document.getElementById('half-circle-spinner');
+		  spinner.style.display = 'block';
+		  const data = await this.fetchData('getClassDetails?levelId=');
+		  var $this = this;
+		  var listsData = data[0];
+		  this.viewClassLocations(listsData);
+		  
+		} catch (error) {
+			console.error('Error rendering random number:', error);
+		}
+	}
+}
