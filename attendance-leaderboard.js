7b1d3e7b54e3c80f9a3c31be2efd62fda121ff17
sk1840939 <63837566+sk1840939@users.noreply.github.com>
2023-07-18 00:04:42 -0700

    Update attendance-leaderboard.js

--- attendance-leaderboard.js
+++ attendance-leaderboard.js
@@ -1,1 +1,34 @@
-
+/**
+ * 	
+ * @param name - HTML element name
+ * @param className - HTML element class attribute
+ * @param idName - HTML element id attribute
+ */
+function creEl(name,className,idName){
+  var el = document.createElement(name);
+	if(className){
+	  el.className = className;
+	}
+	if(idName){
+	  el.setAttribute("id", idName)
+	}
+	return el;
+}
+/**
+ * Class for handling single form
+ * @param webflowMemberId - memberId
+ * @param responseText - single form object provided by API
+ * @param currentIndex - current index for form object
+ * @param accountEmail - email id of member
+ */
+class showLeaderboard {
+	constructor(webflowMemberId,responseText){
+		this.webflowMemberId = webflowMemberId;
+		this.renderLeadboard(responseText);
+	}
+  
+	renderLeaderboard(responseText) {
+    output = responseText.output;
+    console.log(output);    
+    
+  }
