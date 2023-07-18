03d4d6cd9e3ec021bc1ec29b9f0489853c6d5667
sk1840939 <63837566+sk1840939@users.noreply.github.com>
2023-07-18 00:52:03 -0700

    Minor debug

--- attendance-leaderboard.js
+++ attendance-leaderboard.js
@@ -33,53 +33,56 @@ renderLeaderboard(responseText) {
 	const output = responseText.output;
 	console.log(output);
 
-	// Create Overall Div
-	const gridDiv = document.createElement('div');
-	gridDiv.classList.add('w-layout-grid', 'leaderboard-grid');
+	if (output.length !== 0) {
+		// Create Overall Div
+		const gridDiv = document.createElement('div');
+		gridDiv.classList.add('w-layout-grid', 'leaderboard-grid');
+		
+	
+		// Header Row
+		const LabHeader = document.createElement('p');
+		paragraphElement.classList.add('center', 'bold-text', 'dm-sans');
+		paragraphElement.textContent = 'Lab';
+		gridDiv.appendChild(LabHeader);
+	
+		const ScoreHeader = document.createElement('p');
+		paragraphElement.classList.add('center', 'bold-text', 'dm-sans');
+		paragraphElement.textContent = 'Score';
+		gridDiv.appendChild(ScoreHeader);
+	
+		// Lab Score Rows
+		for (const item of output) {
+			// Access the properties of each item within the array
+			const labName = item.labName;
+			const count = item.count; 
+	
+			// Create lab and score divs
+			const labDiv = document.createElement('div');
+			scoreDiv.classList.add('div-block-21');
+	
+			const labText = document.createElement('p');
+			paragraphText.classList.add('center margin-top-10px');
+			paragraphText.textContent(labName);
+	
+			labDiv.appendChild(labText);
+	
+			const scoreDiv = document.createElement('div');
+			scoreDiv.classList.add('div-block-21');
+	
+			const scoreText = document.createElement('p');
+			paragraphText.classList.add('center margin-top-10px');
+			paragraphText.textContent(count);
+	
+			scoreDiv.appendChild(scoreText);
+	
+			// Add to grid
+			gridDiv.appendChild(labDiv);
+			gridDiv.appendChild(scoreDiv);
+		}
 
-	// Header Row
-	const LabHeader = document.createElement('p');
-	paragraphElement.classList.add('center', 'bold-text', 'dm-sans');
-	paragraphElement.textContent = 'Lab';
-	gridDiv.appendChild(LabHeader);
-
-	const ScoreHeader = document.createElement('p');
-	paragraphElement.classList.add('center', 'bold-text', 'dm-sans');
-	paragraphElement.textContent = 'Score';
-	gridDiv.appendChild(ScoreHeader);
-
-	// Lab Score Rows
-	for (const item of output) {
-		// Access the properties of each item within the array
-		const labName = item.labName;
-		const count = item.count; 
-
-		// Create lab and score divs
-		const labDiv = document.createElement('div');
-		scoreDiv.classList.add('div-block-21');
-
-		const labText = document.createElement('p');
-		paragraphText.classList.add('center margin-top-10px');
-		paragraphText.textContent(labName);
-
-		labDiv.appendChild(labText);
-
-		const scoreDiv = document.createElement('div');
-		scoreDiv.classList.add('div-block-21');
-
-		const scoreText = document.createElement('p');
-		paragraphText.classList.add('center margin-top-10px');
-		paragraphText.textContent(count);
-
-		scoreDiv.appendChild(scoreText);
-
-		// Add to grid
-		gridDiv.appendChild(labDiv);
-		gridDiv.appendChild(scoreDiv);
+		// Add to Webflow DOM
+		const leaderDiv = document.getElementById('attendanceLeaderboard');
+		leaderDiv.appendChild(gridDiv); 
 	}
-
-	// Add to Webflow DOM
-	const leaderDiv = document.getElementById('attendanceLeaderboard');
-	leaderDiv.appendChild(gridDiv); 
   }
 }
