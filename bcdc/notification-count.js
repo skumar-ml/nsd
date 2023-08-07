ee4f02ffc955580a7a17fb7c24ffe02a495e7c35
yogii30 <92358002+yogii30@users.noreply.github.com>
2023-08-07 15:27:00 +0530

    Add files via upload

---
+++ bcdc/notification-count.js
@@ -0,0 +1,35 @@
+class NotificationCount {
+	constructor(webflowMemberId){
+		this.webflowMemberId = webflowMemberId;
+		this.getNotificationData();
+	}
+	displayUnreadMessage(messageData){
+		var notificationBudge = document.getElementsByClassName("notification-budge")[0];
+		var notificationCount = document.getElementsByClassName("notification-count")[0];
+		if(notificationBudge){
+			if(notificationCount){
+				notificationCount.remove();
+			}
+			var unreadMessage = messageData.filter(data => !data.is_read)
+			var notificationMessage = creEl('span', 'notification-count');
+			notificationMessage.innerHTML = unreadMessage.length;
+			//notificationBudge.setAttribute('data-count', unreadMessage.length);
+			notificationBudge.appendChild(notificationMessage)
+		}
+	}
+	getNotificationData(){
+		var xhr = new XMLHttpRequest()
+		var $this = this;
+		xhr.open("GET", "https://73u5k1iw5h.execute-api.us-east-1.amazonaws.com/prod/camp/getNotifications//"+$this.webflowMemberId, true)
+		xhr.withCredentials = false
+		xhr.send()
+		xhr.onload = function() {
+			let responseText =  JSON.parse(xhr.responseText);
+			$this.displayUnreadMessage(responseText)
+		}
+	}
+}
+
+(function() {
+    new NotificationCount(webflowMemberId)
+})();
