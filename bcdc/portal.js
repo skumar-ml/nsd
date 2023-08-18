7e1241ff117fb883d007d5bdd6fcd4d5ee11804f
yogii30 <92358002+yogii30@users.noreply.github.com>
2023-08-18 18:43:35 +0530

    Update portal.js

--- bcdc/portal.js
+++ bcdc/portal.js
@@ -246,7 +246,7 @@ class portalForm {
 				   }
 				   
 			   }else{
-					var formLink=creEl('a');
+					var formLink=creEl('a', (editable)? 'completed_form_link': '');
 					//formLink.href = "https://www.jotform.com/submission/"+invoice.submissionId;
 					//Add iframe when it's live and above certain screenwidth
 					//formLink.className = (window.innerWidth > 1200) ? "iframe-lightbox-link" : "";
