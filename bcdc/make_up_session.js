8830410c12a7a858b2e46831e9f8583a123e6e3d
yogii30 <92358002+yogii30@users.noreply.github.com>
2023-08-22 12:52:38 +0530

    Added Comments

--- bcdc/make_up_session.js
+++ bcdc/make_up_session.js
@@ -14,7 +14,11 @@ function creEl(name,className,idName){
 	}
 	return el;
 }
-
+/**
+ * Used PortalTabs class name to integrate with portal js easily.
+ * In this API we pass apiBaseUrl, webflowMemberId, accountEmail.
+ * In this class we are manipulating student data and creating makeup session links for students
+ */
 class PortalTabs {
 	
 	constructor(apiBaseUrl, webflowMemberId,accountEmail) {
@@ -23,27 +27,30 @@ class PortalTabs {
 		this.accountEmail = accountEmail;
 		this.renderPortalData();
 	}
-	
+	// Passing all student data and creating student link
 	createMakeUpSession(data){
-		console.log('data', data)
+		// Getting main dom element object to add student list with link
 		var studentList = document.getElementById('make_up_session');
 		studentList.innerHTML = "";
 		data.forEach((sData)=>{
+			// Getting single student list
 			var sList = this.createStudentList(sData);
 				studentList.appendChild(sList);
 		})
 	}
+	// Manipulating single student list
 	createStudentList(studentData){
 		var wLayoutGrid = creEl('div', 'w-layout-grid make-student-data');
 		var sName = creEl('h4')
 		
 		var classLevel = (studentData.classDetail != null)? studentData.classDetail.classLevel : '';
+		// Creating student name with class details
 		sName.innerHTML = studentData.studentDetail.studentName+" ( "+classLevel+" - "+studentData.classLoactionDeatils.locationName+' - '+studentData.classDetail.startTime+")";
 				
-		//sName.innerHTML = studentData.studentDetail.studentName
 		var btnSection = creEl('div', 'link-container');
 		var link = creEl('a', 'main-button w-button')
 		link.innerHTML = "Add Make Up Session";
+		// Passing dynamic data to acuity scheduling link
 		link.href = 'https://bergendebate.as.me/schedule.php?appointmentType=51374724&field:12876955='+studentData.studentDetail.studentName+'&field:13727280='+studentData.studentDetail.parentEmail;
 		link.target = '_blank';
 		btnSection.appendChild(link)
@@ -51,7 +58,7 @@ class PortalTabs {
 		wLayoutGrid.appendChild(btnSection)
 		return wLayoutGrid;
 	}
-	
+	// Get API data with the help of an endpoint
 	async fetchData(endpoint) {
 		try {
 			const response = await fetch(`${this.baseUrl}${endpoint}`);
@@ -65,6 +72,7 @@ class PortalTabs {
 			throw error;
 		}
 	}
+	// After the API response we call the createMakeUpSession method to manipulate student data
 	async renderPortalData(memberId) {
 		try {
 		  const data = await this.fetchData('getInvoiceDetail/'+this.webflowMemberId);
