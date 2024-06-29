25e0b9ff965e6da869ced4f6959e6171de44d848
Shubham Kumar <63837566+skumar-ml@users.noreply.github.com>
2024-06-28 23:52:54 -0700

    Update calendar links for v1 of camps 2024

--- coach-portal.js
+++ coach-portal.js
@@ -7,37 +7,37 @@ async function coachResources(coachCampId, memberId) {
   // Texas PF
   if (coachCampId === "1") {
     coachCampText.textContent = "Texas PF";
-    scheduleLink = "https://docs.google.com/spreadsheets/d/1Hsyctp2rHDcFM0HTdsBpfEgn2uuFQuuN_sonIBJTVUg/edit?usp=sharing";
+    scheduleLink = "https://calendar.google.com/calendar/u/1/embed?src=texaspf@nsdebatecamp.com&ctz=America/Chicago&csspa=1";
   }
   // Texas LD
   else if (coachCampId === "2") {
     coachCampText.textContent = "Texas LD";
-    scheduleLink = "https://docs.google.com/spreadsheets/d/1sRCxAjMODRSbjtewP0ra8iGRTVVs1DyTnKYQw6SxhFc/edit?usp=sharing";
+    scheduleLink = "https://calendar.google.com/calendar/embed?src=66f1fdbfd5235514cb5f1c1faf0a1d3c3066eb0b8efb64d6ae44c65ad0ed0b20%40group.calendar.google.com&ctz=America%2FChicago";
   }
   // Philadelphia PF
   else if (coachCampId === "3") {
-    coachCampText.textContent = "Philadelphia PF";
-    scheduleLink = "https://docs.google.com/spreadsheets/d/1ATEdPhP6cyOWhtiM88KWkgR2OBMrlLiYzZUlgnoHZS0/edit?usp=sharing";
+    coachCampText.textContent = "Philadelphia PF Session 1";
+    scheduleLink = "https://calendar.google.com/calendar/embed?src=287e33ec1e3bf1ccea3e84481a3d594eb3d5906f177b7967993013b497f8dd29%40group.calendar.google.com&ctz=America%2FIndiana%2FIndianapolis";
   }
   // Philadelphia LD
   else if (coachCampId === "4") {
     coachCampText.textContent = "Philadelphia LD";
-    scheduleLink = "https://docs.google.com/spreadsheets/d/13vdr2zF8x-KTI88XV7D3zSFr5CBlZ10lmlUOnIrYsag/edit?usp=sharing";
+    scheduleLink = "https://calendar.google.com/calendar/u/0/embed?src=5aec18b59661c93db59df9117e1072349a00a1edc519a62e2548ec42d070db92@group.calendar.google.com&ctz=America/New_York";
   }
   // Minnesota LD
   else if (coachCampId === "5") {
     coachCampText.textContent = "Minnesota LD";
-    scheduleLink = "https://docs.google.com/spreadsheets/d/1xd4zkg5wYn6_TbO3JfG2PLrHoVNrkTGsaGvACvAsJAQ/edit?usp=sharing";
+    // scheduleLink = "https://docs.google.com/spreadsheets/d/1xd4zkg5wYn6_TbO3JfG2PLrHoVNrkTGsaGvACvAsJAQ/edit?usp=sharing";
   }  
      // Online LD Camp
   else if (coachCampId === "7") {
     coachCampText.textContent = "Online LD Camp";
-    scheduleLink = "https://docs.google.com/spreadsheets/d/1v6A-0xbqEq562VC72QbEFEfRJfFrg8jajbK3FN0R5oQ/edit?usp=sharing";
+    // scheduleLink = "https://docs.google.com/spreadsheets/d/1v6A-0xbqEq562VC72QbEFEfRJfFrg8jajbK3FN0R5oQ/edit?usp=sharing";
   }  
    // Online PF Camp
   // else if (coachCampId === "8") {
   //   coachCampText.textContent = "Online PF Camp";
-  //   scheduleLink = "https://docs.google.com/spreadsheets/d/1dTC-_Z7aiHJOoJAlMkLdcFnG-q9dTuZPkmuvlaPzy48/edit#gid=0";
+  //   scheduleLink = "https://calendar.google.com/calendar/embed?src=9aaf227f1b5184079588883e3b512c53504825638023a74ee30d8488b9dc22e8%40group.calendar.google.com&ctz=America%2FIndiana%2FIndianapolis";
   // }  
   var scheduleHTML = '<a href=' + scheduleLink + ' target="_blank" class="portal-resource-card portal-div-shadow w-inline-block"><p class="portal-card-text">Camp Schedule</p></a>';
   coachResourceGrid.insertAdjacentHTML('beforeend', scheduleHTML)
