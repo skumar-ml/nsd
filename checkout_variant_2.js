718abea75b0112f9e5f16140b29b8f47bc76be90
yogii30 <92358002+yogii30@users.noreply.github.com>
2024-12-05 11:33:24 +0530

    Update checkout_variant_2.js

--- checkout_variant_2.js
+++ checkout_variant_2.js
@@ -324,9 +324,9 @@ class CheckOutWebflow {
 		// Hide and show based on supplementary program length
 		var totalPriceDiv = document.getElementById("totalPriceDiv");
 		if (selectedIds.length > 0) {
-			totalPriceDiv.style.display = "block";
+			totalPriceDiv.classList.add('show');
 		} else {
-			totalPriceDiv.style.display = "none";
+			totalPriceDiv.classList.remove('show');
 		}
 		// Hide and show based on supplementary program length
 		var totalPriceDiv = document.getElementById("totalPriceDivMob");
