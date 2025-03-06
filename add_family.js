1f5ecfb46d6ce6207079ecca7b172517be98930b
yogii30 <92358002+yogii30@users.noreply.github.com>
2025-03-06 20:27:42 +0530

    Add Family Member Feature Added

---
+++ add_family.js
@@ -0,0 +1,226 @@
+class FamilyMember {
+  $editMemberData = [];
+  constructor(data) {
+    this.memberId = data.memberId;
+    this.baseUrl = data.baseUrl;
+    this.displayFamilyMember();
+    this.handleEditMember();
+  }
+  // Get API data with the help of endpoint
+  async fetchData(endpoint) {
+    try {
+      const response = await fetch(`${this.baseUrl}${endpoint}`);
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      throw error;
+    }
+  }
+  /**
+   *
+   * @param name - HTML element name
+   * @param className - HTML element class attribute
+   * @param idName - HTML element id attribute
+   */
+  creEl(name, className, idName) {
+    var el = document.createElement(name);
+    if (className) {
+      el.className = className;
+    }
+    if (idName) {
+      el.setAttribute("id", idName);
+    }
+    return el;
+  }
+  /**
+   * Get family member data based on memberId
+   */
+  async displayFamilyMember() {
+    var spinner = document.getElementById("half-circle-spinner");
+		spinner.style.display = "block";
+    const familyData = await this.fetchData(
+      "getAllFamilyData/" + this.memberId
+    );
+    console.log("family Data", familyData);
+    const FamilyMemberWrapper = document.querySelector(
+      ".family-member-grid-wrapper"
+    );
+    FamilyMemberWrapper.innerHTML = "";
+    familyData.forEach((element) => {
+      let singleFamily = this.getSingleFamilyData(element);
+      FamilyMemberWrapper.appendChild(singleFamily);
+    });
+    spinner.style.display = "none";
+  }
+
+  /**
+   * Manipulate single family data and return single family html
+   */
+  getSingleFamilyData(data) {
+    var $this = this;
+    const familyMemberContainer = this.creEl(
+      "div",
+      "family-member-vertical-flex-container"
+    );
+
+    const nameParagraph = this.creEl("p", "dm-sans bold-text");
+    nameParagraph.innerHTML = data.firstName + " " + data.lastName + "<br>";
+    familyMemberContainer.appendChild(nameParagraph);
+
+    const emailLink = this.creEl("a");
+    emailLink.href = "#";
+    emailLink.classList.add("w-inline-block");
+    const emailParagraph = this.creEl("p", "dm-sans");
+    emailParagraph.textContent = data.email;
+    emailLink.appendChild(emailParagraph);
+    familyMemberContainer.appendChild(emailLink);
+
+    const roleParagraph = this.creEl("p", "dm-sans italics");
+    roleParagraph.innerHTML = data.accountType + "<br>";
+    familyMemberContainer.appendChild(roleParagraph);
+
+    const createdOnParagraph = this.creEl("p", "dm-sans");
+    createdOnParagraph.textContent = "Created On - 06-03-2025";
+    familyMemberContainer.appendChild(createdOnParagraph);
+
+    const editButton = this.creEl(
+      "a",
+      "main-button transparent-red edit-btn w-button"
+    );
+    editButton.href = "#";
+    editButton.textContent = "Edit";
+    editButton.addEventListener("click", function () {
+      $this.$editMemberData = data;
+      $this.updateSuppData(data);
+      const addFamilyMemberEditModals = document.querySelector(
+        ".add-family-member-edit-modal"
+      );
+      addFamilyMemberEditModals.classList.add("show");
+      addFamilyMemberEditModals.style.display = "flex";
+    });
+    familyMemberContainer.appendChild(editButton);
+
+    // return the constructed element to parent container
+    return familyMemberContainer;
+  }
+
+  // Update student data for addon supplementary program purchase
+  updateSuppData(data) {
+    var studentFirstName = document.getElementById("Student-First-Name");
+    var studentLastName = document.getElementById("Student-Last-Name");
+    var studentEmail = document.getElementById("Student-Email");
+    var studentGrade = document.getElementById("Student-Grade");
+    var parentPhone = document.getElementById("parent-phone");
+    var studentSchool = document.getElementById("Student-School");
+    var studentGender = document.getElementById("Student-Gender");
+    if (data.accountType == "parent") {
+      parentPhone.closest("div").classList.remove("hide");
+      studentGrade.closest("div").classList.add("hide");
+    } else {
+      parentPhone.closest("div").classList.add("hide");
+      studentGrade.closest("div").classList.remove("hide");
+      if(!data.itemId){
+        studentGrade.closest("div").classList.add("hide");
+      }else{
+        studentGrade.closest("div").classList.remove("hide");
+      }
+    }
+    
+    if (data != null) {
+      studentEmail.value = data.email;
+      studentFirstName.value = data.firstName;
+      studentLastName.value = data.lastName;
+
+      if (data.studentGrade) {
+        studentGrade.value = data.studentGrade;
+      }
+      if (data.parentPhoneNumber) {
+        parentPhone.value = data.parentPhoneNumber;
+      }
+    }
+  }
+
+  handleEditMember() {
+    var $this = this;
+    const editMemberBtn = document.getElementById("editMemberBtn");
+    editMemberBtn.addEventListener("click", function () {
+      editMemberBtn.innerHTML = "Processing";
+      editMemberBtn.classList.add("disabled");
+      editMemberBtn.style.pointerEvents = "none";
+      $this.editMemberInfo($this.$editMemberData);
+    });
+  }
+  closeModal(modal) {
+    if (modal) {
+      modal.classList.remove("show");
+      modal.style.display = "none";
+    }
+  }
+  editMemberInfo(memberData) {
+    var $this = this;
+    var studentFirstName = document.getElementById("Student-First-Name");
+    var studentLastName = document.getElementById("Student-Last-Name");
+    var studentEmail = document.getElementById("Student-Email");
+    var studentGrade = document.getElementById("Student-Grade");
+    var parentPhone = document.getElementById("parent-phone");
+    const editMemberBtn = document.getElementById("editMemberBtn");
+    var data = {
+      memberId: memberData.memberId ? memberData.memberId : "",
+      oldLastName: memberData.lastName,
+      oldFirstName: memberData.firstName,
+      oldEmailId: memberData.email,
+      oldPhoneNo: memberData.parentPhoneNumber,
+      oldGrade: memberData.studentGrade,
+      newEmailId: studentEmail.value,
+      newFirstName: studentFirstName.value,
+      newLastName: studentLastName.value,
+      newPhoneNo: parentPhone.value,
+      newGrade: studentGrade.value,
+      itemId: memberData.itemId ? memberData.itemId : "",
+    };
+    console.log("data", data);
+    var xhr = new XMLHttpRequest();
+    var $this = this;
+    xhr.open(
+      "POST",
+      "https://3yf0irxn2c.execute-api.us-west-1.amazonaws.com/dev/camp/updateMemberStack",
+      true
+    );
+    xhr.withCredentials = false;
+    xhr.send(JSON.stringify(data));
+    xhr.onload = function () {
+      let responseText = JSON.parse(xhr.responseText);
+      console.log(xhr.responseText, responseText);
+      const addFamilyMemberEditModals = document.querySelector(
+        ".add-family-member-edit-modal"
+      );
+      $this.closeModal(addFamilyMemberEditModals);
+      $this.displayFamilyMember();
+      // if (responseText.success) {
+      // 	console.log('success')
+      // } else {
+      // 	reject(new Error('API call failed'));
+      // }
+      editMemberBtn.innerHTML = "Save";
+      editMemberBtn.classList.remove("disabled");
+      editMemberBtn.style.pointerEvents = "auto";
+    };
+    xhr.onreadystatechange = function (oEvent) {
+      if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+          console.log(xhr.responseText);
+        } else {
+          console.log("Error", xhr.statusText);
+          editMemberBtn.innerHTML = "Save";
+          editMemberBtn.classList.remove("disabled");
+          editMemberBtn.style.pointerEvents = "auto";
+        }
+      }
+    };
+  }
+}
+
