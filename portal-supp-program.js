e9b73b791625ddce0677bc1752985ed26fc79f02
yogii30 <92358002+yogii30@users.noreply.github.com>
2025-04-15 18:30:36 +0530

    Added Source

--- portal-supp-program.js
+++ portal-supp-program.js
@@ -524,6 +524,7 @@ class DisplaySuppProgram {
         this.memberData.site_url + "members/" + this.memberData.memberId,
       label: programName,
       amount: parseFloat(amount * 100),
+      source: "success_page"
     };
     // Create the POST request
     fetch(this.memberData.baseUrl + "createCheckoutUrlForSupplementary", {
