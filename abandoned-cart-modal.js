b25ca9073b19c89ddc84e7649c6495e3b9627096
yogii30 <92358002+yogii30@users.noreply.github.com>
2025-05-07 15:58:48 +0530

    Update abandoned-cart-modal.js

--- abandoned-cart-modal.js
+++ abandoned-cart-modal.js
@@ -79,7 +79,7 @@ class AbandonedCartModal {
       const createdOnDate = new Date(data.createdOn);
       //const sixHoursAgo = new Date();
       //sixHoursAgo.setHours(sixHoursAgo.getMinutes() - 5);
-      const sixHoursAgo = new Date(Date.now() - 5 * 60 * 1000);
+      const sixHoursAgo = new Date(Date.now() - this.data.hour * 60 * 60 * 1000);
       // Condition 1: If abandoned cart happened less than 6 hours ago
       if (createdOnDate > sixHoursAgo) {
         console.log(
@@ -140,7 +140,7 @@ class AbandonedCartModal {
         const lastOpenedDate = new Date(localStorage.getItem("lastModalClosedDate"));
         const now = new Date();
         const sevenDaysLater = new Date(lastOpenedDate);
-        sevenDaysLater.setDate(lastOpenedDate.getDate() + 1);
+        sevenDaysLater.setDate(lastOpenedDate.getDate() + 7);
 
         if (now < sevenDaysLater) {
           console.log(
